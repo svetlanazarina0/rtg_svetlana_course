75eaaa258c132e8484a90832ea68825b5de5b4dd
Svetlana Zarina <svetlanazarina0@gmail.com>
2022-09-12 20:57:25 +0300

    exception for first task

--- 3_homework.py
+++ 3_homework.py
@@ -1,6 +1,14 @@
 
-
+#ex1
+ 
 user_temperature= input("Please enter your temperature ")
+
+try:
+    float(user_temperature)
+except ValueError:
+    print("Please enter valid number. Decimals should be seperated with .")
+  
+
 user_temperature= float(user_temperature)
 type(user_temperature)
 print(user_temperature)
@@ -18,7 +26,15 @@ else :
 
 
 
- 
+ #ex2
+ # 2. Xmas Bonus
+# The company has promised a Christmas bonus in the amount of 15% of the monthly salary for EVERY year of 
+# service over 2 years.
+# Task. Ask the user for the amount of the monthly salary and the number of years worked.
+# Calculate the bonus.
+# Example1: 5 years of experience, 1000 Euro salary, the bonus will be 450 Euro.
+# Example2: 1.5 years of experience, 1500 Euro salary, no bonus(0)
+
 salary = float(input("What is your montly salary? "))
 years = float(input("Please enter number of years you have worked "))
 bonus_perc = 0.15
