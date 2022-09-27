05930165de19ad82ef2590205f14e140ae5069bb
Svetlana Zarina <svetlanazarina0@gmail.com>
2022-09-27 20:52:58 +0300

    9 hw

---
+++ 9_homework.py
@@ -0,0 +1,30 @@
+# 1. Min, Avg, Max
+# Write a function get_min_avg_max (sequence) that returns a tuple with three values, the smallest, the arithmetic mean, and the largest value in the string, respectively.
+# Example:
+# get_min_avg_max ([0,10,1,9]) -> (0,5,10)
+# the incoming sequence can be a tuple or a list with numeric values.
+import statistics
+my_nums = [int(t) for t in input("Enter values separated by whitespace").split(" ")]
+#print(my_nums, min(my_nums), statistics.mean(my_nums), max(my_nums))
+values= min(my_nums), statistics.mean(my_nums), max(my_nums)
+tuple_values=tuple(values)
+print(tuple_values)
+#not a function..
+
+# Write a function that returns a tuple with common elements in three sequences. Inputs can be list, tuple, string.
+# get_common_elements(seq1, seq2, seq3)
+# Example:
+# get_common_elements("abc", ['a', 'b'], ('b', 'c')) -> ('b',) # we return a tuple with a single element 
+# # remember that we can convert strings to set with set(mystring), and set to tuple with tuple(myset)
+# 2. b For those with some experience 
+# BONUS:  make a function that can handle an arbitrary number of input sequences
+# so function which takes any number of sequences and returns a tuple with common elements
+# get_common_elements(seq1, seq2, seq3, seq4, seq5, seq6, seq7) etc :), so just like print takes any number of values
+
+
+def get_common_elements(s1,s2,s3):
+     return tuple(set(s1).intersection(set(s2).intersection(set(s3))))
+     
+ x, y, z = input("Enter three sequences, seperated by a whitespace: ").split()
+get_common_elements(x,y,z)
+
